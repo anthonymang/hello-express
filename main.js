d141b967ef5b8af12b701721433a442d0add019e
anthonymang <anthonymanganielloda@gmail.com>
2021-05-05 11:03:26 -0400

    add 404, api capabilities

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express(); // Instance of the app, server
+
+const axios = require('axios');
+
 
 // Home route
 app.get('/', function(req, res){
@@ -11,9 +13,43 @@ app.get('/', function(req, res){
     res.send('Hello, World!')
 });
 
-app.get('/sei', (req, res) => {
-    res.send('SEI 412')
+
+
+app.get('/rockets', (req,res) => {
+    axios.get('https://api.spacexdata.com/v3/rockets')
+    .then(response =>{
+        console.log(response.data)
+        res.json(response.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+app.get('/greet/:name', (req, res) => {
+    console.log(req.params); // req.params (object)
+    res.send(`Hello ${req.params.name}`)
 });
+
+app.get('/github/:username', (req, res) => {
+    const { username } = req.params; // destructuring
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(response=>{
+        console.log(response.data);
+        res.json(response.data);
+    })
+})
+
+
+// app.get('/sei', (req, res) => {
+//     res.send('SEI 412')
+// });
+
+// needs to be last route or else it will intercept another route
+app.get('/*', (req, res) => {
+    console.log(req.params)
+    res.send('404 Error')
+})
 
 const PORT = process.env.PORT || 8000;
 
